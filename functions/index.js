a3b8dcd3b664dc4adc52f99a64077a75350c760a
Deepankar Sawhney <sawhney.deepankar@gmail.com>
2021-10-16 11:35:26 +0530

    increase query size

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,23 @@
 const functions = require("firebase-functions");
+const Filter = require('bad-words');
+
+const admin =require('firebase-admin');
+admin.initializeApp();
+
+const db=admin.firestore();
+
+exports.detectEvilUsers =functions.firestore
+    .document('messages/{msgID}')
+    .onCreate(async (doc,ctx) => {
+        const filter =new Filter();
+        const {text,uid} =doc.data();
+
+        if (filter.isProfane(text)){
+            const cleaned =filter.clean(text);
+            await doc.ref.update({text:`i got banned for life ... ${cleaned}`});
+            await db.collection('banned').doc(uid).set({});
+        }
+    })
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
